8c7f8fae296d2a69eae5e6588bdef70625b1c2ff
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-02-27 15:27:39 +0200

    Removed fixed-style class

--- JS/Nav.js
+++ JS/Nav.js
@@ -2,7 +2,6 @@ window.addEventListener('load', function() {
   var menuBtn = document.querySelector('.menu-button');
 
   menuBtn.addEventListener('click', function() {
-    menuBtn.classList.add('fixed-style');
     menuBtn.textContent = menuBtn.textContent === 'Menu' ? 'Close' : 'Menu';
   });
 });
