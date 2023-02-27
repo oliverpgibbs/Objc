2ee8e66817ae7b4ae17afb32a4e7538cbc258efc
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-02-27 12:55:36 +0200

    Initial commit

---
+++ JS/Nav.js
@@ -0,0 +1,6 @@
+var menuBtn = document.querySelector('.menu-button');
+
+menuBtn.addEventListener('click', function() {
+  menuBtn.classList.add('fixed-style');
+  menuBtn.textContent = menuBtn.textContent === 'Menu' ? 'Close' : 'Menu';
+});
