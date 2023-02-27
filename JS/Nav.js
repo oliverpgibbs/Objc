0fdf200c83b0452c33d58a6970f594c513c9f2c6
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-02-27 15:25:15 +0200

    Update Nav.js

--- JS/Nav.js
+++ JS/Nav.js
@@ -1,6 +1,8 @@
-var menuBtn = document.querySelector('.navbar.nav-container.nav-content.menu-button');
+window.addEventListener('load', function() {
+  var menuBtn = document.querySelector('.menu-button');
 
-menuBtn.addEventListener('click', function() {
-  menuBtn.classList.add('fixed-style');
-  menuBtn.textContent = menuBtn.textContent === 'Menu' ? 'Close' : 'Menu';
+  menuBtn.addEventListener('click', function() {
+    menuBtn.classList.add('fixed-style');
+    menuBtn.textContent = menuBtn.textContent === 'Menu' ? 'Close' : 'Menu';
+  });
 });
