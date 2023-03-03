1e309b9711afcb821b12e1c477391f4275d0f76f
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-03 10:18:41 +0200

    Create Home.js

---
+++ JS/Home.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+  var lottieAnimation = document.getElementById('.lottie-animation.nav-logo');
+  lottieAnimation.removeAttribute('data-autoplay');
+});
