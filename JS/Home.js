c7bd61136038faa8575dfeb8007e017c8951c823
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-03 11:32:35 +0200

    Delete Home.js

--- JS/Home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var lottieAnimation = document.querySelector('.lottie-animation.nav-logo');
-  var animation = lottie.loadAnimation({
-      container: lottieAnimation,
-      renderer: 'svg',
-      path: 'Animations/logo-animation.json',
-      autoplay: false,
-  });
-});
