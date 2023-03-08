df980cdfb753d3543afc5958d4b4d30fcf82022c
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-08 13:56:52 +0200

    Update Book-Detail.js

--- JS/Book-Detail.js
+++ JS/Book-Detail.js
@@ -1,12 +1,17 @@
 window.addEventListener('load', function() {
   var radioButtons = document.querySelectorAll('.book-radio');
   var buyButton = document.querySelector('.buy-book-button');
+  var orButton = document.querySelector('.or-see-bundles-button');
 
   radioButtons.forEach(radioButton => {
     radioButton.addEventListener('click', function() {
         buyButton.style.backgroundColor = '#000000';
         buyButton.style.color = '#FFFFFF';
         buyButton.textContent = 'Buy through Gumroad';
+
+        orButton.style.backgroundColor = '#000000';
+        orButton.style.color = '#FFFFFF';
+        orButton.textContent = 'Buy through Gumroad';
 
         console.log('LOGGED');
     });
