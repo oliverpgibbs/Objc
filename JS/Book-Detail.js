3f6c0dec18bf49ee5b1bfe51848d0a646f94deaf
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-08 14:04:18 +0200

    Update Book-Detail.js

--- JS/Book-Detail.js
+++ JS/Book-Detail.js
@@ -2,14 +2,13 @@ window.addEventListener('load', function() {
   var radioButtons = document.querySelectorAll('.book-radio');
   var buyButton = document.querySelector('.buy-book-button');
 
-  if (buyButton) {
-    console.log('Element found!');
-  } else {
-    console.log('Element not found.');
-  }
+  console.log('Radio buttons found:', radioButtons.length);
+  console.log('Buy button found:', buyButton);
 
   radioButtons.forEach(radioButton => {
     radioButton.addEventListener('change', event => {
+      console.log('Radio button changed:', event.target.value);
+
       if (event.target.checked) {
         buyButton.style.backgroundColor = '#000000';
         buyButton.style.color = '#FFFFFF';
