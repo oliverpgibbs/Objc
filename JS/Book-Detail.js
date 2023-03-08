a12c67f0f79976f87bf21205285bdf7f088fcd63
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-08 14:13:49 +0200

    Update Book-Detail.js

--- JS/Book-Detail.js
+++ JS/Book-Detail.js
@@ -2,18 +2,16 @@ window.addEventListener('load', function() {
   var radioButtons = document.querySelectorAll('.book-radio');
   var buyButton = document.querySelector('.buy-book-button.submit');
 
-  console.log('Radio buttons found:', radioButtons.length);
-  console.log('Buy button found:', buyButton);
-
   radioButtons.forEach(radioButton => {
     radioButton.addEventListener('change', event => {
-      console.log('Radio button changed:', event.target.value);
 
       if (event.target.checked) {
+        buyButton.style.transition = 'all 200ms ease-out';
         buyButton.style.backgroundColor = '#000000';
         buyButton.style.color = '#FFFFFF';
         buyButton.textContent = 'Buy through Gumroad';
       } else {
+        buyButton.style.transition = 'all 200ms ease-out';
         buyButton.style.backgroundColor = '#D3D6D6';
         buyButton.style.color = '#666666';
         buyButton.textContent = 'Make a selection';
