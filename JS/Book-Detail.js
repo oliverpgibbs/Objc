9edee71cca6963a82f4a88aaa0b6a0c2c8739a4d
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-08 13:02:09 +0200

    Update Book-Detail.js

--- JS/Book-Detail.js
+++ JS/Book-Detail.js
@@ -1,20 +1,22 @@
-const radioButtons = document.querySelectorAll('.book-radio');
-const buyButton = document.querySelector('.buy-book-button');
+window.addEventListener('load', function() {
+  const radioButtons = document.querySelectorAll('.book-radio');
+  const buyButton = document.querySelector('.buy-book-button');
 
-radioButtons.forEach(radioButton => {
-  radioButton.addEventListener('change', event => {
-    if (event.target.checked) {
-      buyButton.style.backgroundColor = '#000000';
-      buyButton.style.color = '#FFFFFF';
-      buyButton.textContent = 'Buy through Gumroad';
+  radioButtons.forEach(radioButton => {
+    radioButton.addEventListener('change', event => {
+      if (event.target.checked) {
+        buyButton.style.backgroundColor = '#000000';
+        buyButton.style.color = '#FFFFFF';
+        buyButton.textContent = 'Buy through Gumroad';
 
-      console.log('CHECKED');
-    } else {
-      buyButton.style.backgroundColor = '#D3D6D6';
-      buyButton.style.color = '#666666';
-      buyButton.textContent = 'Make a selection';
+        console.log('CHECKED');
+      } else {
+        buyButton.style.backgroundColor = '#D3D6D6';
+        buyButton.style.color = '#666666';
+        buyButton.textContent = 'Make a selection';
 
-      console.log('UNCHECKED');
-    }
+        console.log('UNCHECKED');
+      }
+    });
   });
 });
