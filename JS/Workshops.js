658f628aedd3976ee752bd10d3ce0aeee8781bdc
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-10 14:52:28 +0200

    Update Workshops.js

--- JS/Workshops.js
+++ JS/Workshops.js
@@ -1,12 +1,27 @@
 window.addEventListener('load', function() {
-  var whatWeCoverDropdownToggle = document.querySelector('.what-we-cover-dropdown-toggle');
-  var toggleIcon = document.querySelector('.h5.mobile.toggle-icon');
+  var whatWeCoverDropdownToggles = document.querySelectorAll('.what-we-cover-dropdown-toggle');
+  var toggleIcon = document.querySelector('.h5.mobile.toggle-icon.workshops');
 
-  whatWeCoverDropdownToggle.addEventListener('click', function() {
-    if (whatWeCoverDropdownToggle.classList.contains('w--open')) {
-      toggleIcon.textContent = '+'
-    } else {
-      toggleIcon.textContent = '–'
-    }
-  });
+  whatWeCoverDropdownToggles.forEach(whatWeCoverDropdownToggle => {
+    whatWeCoverDropdownToggle.addEventListener('click', function() {
+      if (whatWeCoverDropdownToggle.classList.contains('w--open')) {
+        toggleIcon.textContent = '+'
+      } else {
+        toggleIcon.textContent = '–'
+      }
+    });
+  }
+
+  var faqDropdownToggles = document.querySelectorAll('.workshops-faq-dropdown-toggle');
+  var faqToggleIcon = document.querySelector('.h5.mobile.toggle-icon.faq');
+
+  faqDropdownToggles.forEach(faqDropdownToggle => {
+    faqDropdownToggle.addEventListener('click', function() {
+      if (faqDropdownToggle.classList.contains('w--open')) {
+        faqToggleIcon.textContent = '+'
+      } else {
+        faqToggleIcon.textContent = '–'
+      }
+    });
+  }
 });
