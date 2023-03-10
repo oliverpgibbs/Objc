b4b7ebd9f62a37c8cf8493eff037f24a8f7de873
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-10 15:11:57 +0200

    Update Workshops.js

--- JS/Workshops.js
+++ JS/Workshops.js
@@ -1,4 +1,7 @@
 window.addEventListener('load', function() {
+  var activeWhatWeCoverDropdownToggle = null;
+  var activeFaqDropdownToggle = null;
+
   var whatWeCoverDropdownToggles = document.querySelectorAll('.what-we-cover-dropdown-toggle');
   var faqDropdownToggles = document.querySelectorAll('.workshops-faq-dropdown-toggle');
 
@@ -6,16 +9,22 @@ window.addEventListener('load', function() {
     whatWeCoverDropdownToggle.addEventListener('click', function() {
       var toggleIcon = whatWeCoverDropdownToggle.closest('.what-we-cover-dropdown').querySelector('.h5.mobile.toggle-icon.workshops');
 
+      if (whatWeCoverDropdownToggle !== activeWhatWeCoverDropdownToggle) {
+        // Only change the text content of other toggle icons if a new dropdown toggle is clicked
+        if (activeWhatWeCoverDropdownToggle) {
+          var otherToggleIcons = activeWhatWeCoverDropdownToggle.closest('.what-we-cover-dropdown').querySelectorAll('.h5.mobile.toggle-icon.workshops');
+          otherToggleIcons.forEach(icon => {
+            icon.textContent = '+';
+          });
+        }
+
+        activeWhatWeCoverDropdownToggle = whatWeCoverDropdownToggle;
+      }
+
       if (whatWeCoverDropdownToggle.classList.contains('w--open')) {
         toggleIcon.textContent = '+';
       } else {
         toggleIcon.textContent = '–';
-
-        // Change the text content of all other toggle icons to '+'
-        var otherToggleIcons = document.querySelectorAll('.h5.mobile.toggle-icon.workshops:not(:first-of-type)');
-        otherToggleIcons.forEach(icon => {
-          icon.textContent = '+';
-        });
       }
     });
   });
@@ -24,17 +33,22 @@ window.addEventListener('load', function() {
     faqDropdownToggle.addEventListener('click', function() {
       var faqToggleIcon = faqDropdownToggle.closest('.workshops-faq-dropdown-toggle').querySelector('.h5.mobile.toggle-icon.faq');
 
+      if (faqDropdownToggle !== activeFaqDropdownToggle) {
+        // Only change the text content of other toggle icons if a new dropdown toggle is clicked
+        if (activeFaqDropdownToggle) {
+          var otherToggleIcons = activeFaqDropdownToggle.closest('.workshops-faq-dropdown-toggle').querySelectorAll('.h5.mobile.toggle-icon.faq');
+          otherToggleIcons.forEach(icon => {
+            icon.textContent = '+';
+          });
+        }
+
+        activeFaqDropdownToggle = faqDropdownToggle;
+      }
+
       if (faqDropdownToggle.classList.contains('w--open')) {
         faqToggleIcon.textContent = '+';
-
       } else {
         faqToggleIcon.textContent = '–';
-
-        // Change the text content of all other toggle icons to '+'
-        var otherToggleIcons = document.querySelectorAll('.h5.mobile.toggle-icon.faq:not(:first-of-type)');
-        otherToggleIcons.forEach(icon => {
-          icon.textContent = '+';
-        });
       }
     });
   });
