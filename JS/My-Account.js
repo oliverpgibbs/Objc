41b62f2492b17efddcf0d0b6d052a31adcc80ee8
Oliver Gibbs <oliverpgibbs@yahoo.com>
2023-03-16 15:16:11 +0200

    Update My-Account.js

--- JS/My-Account.js
+++ JS/My-Account.js
@@ -1,25 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const profileButton = document.querySelector(".my-account-button.profile");
-  const billingButton = document.querySelector(".my-account-button.billing");
-  const profileContainer = document.querySelector(".profile-container");
-  const billingContainer = document.querySelector(".billing-container");
+document.addEventListener('DOMContentLoaded', () => {
+  const profileButton = document.querySelector('.my-account-button.profile');
+  const profileContainer = document.querySelector('.profile-container');
+  const billingButton = document.querySelector('.my-account-button.billing');
+  const billingContainer = document.querySelector('.billing-container');
+  const teamButton = document.querySelector('.my-account-button.team');
+  const teamContainer = document.querySelector('.team-container');
 
   // Set initial state
-  profileButton.classList.add("selected");
-  profileContainer.style.display = "flex";
-  billingContainer.style.display = "none";
+  profileButton.classList.add('selected');
+  profileContainer.style.display = 'flex';
+  billingContainer.style.display = 'none';
 
-  profileButton.addEventListener("click", () => {
-    profileButton.classList.add("selected");
-    billingButton.classList.remove("selected");
-    profileContainer.style.display = "flex";
-    billingContainer.style.display = "none";
+  profileButton.addEventListener('click', () => {
+    profileButton.classList.add('selected');
+    billingButton.classList.remove('selected');
+    teamButton.classList.remove('selected');
+
+    profileContainer.style.display = 'flex';
+    billingContainer.style.display = 'none';
+    teamContainer.style.display = 'none';
   });
 
-  billingButton.addEventListener("click", () => {
-    billingButton.classList.add("selected");
-    profileButton.classList.remove("selected");
-    profileContainer.style.display = "none";
-    billingContainer.style.display = "flex";
+  billingButton.addEventListener('click', () => {
+    profileButton.classList.remove('selected');
+    billingButton.classList.add('selected');
+    teamButton.classList.remove('selected');
+
+    profileContainer.style.display = 'none';
+    billingContainer.style.display = 'flex';
+    teamContainer.style.display = 'none';
+  });
+
+  teamButton.addEventListener('click', () => {
+    profileButton.classList.remove('selected');
+    billingButton.classList.remove('selected');
+    teamButton.classList.add('selected');
+
+    profileContainer.style.display = 'none';
+    billingContainer.style.display = 'none';
+    teamContainer.style.display = 'flex';
   });
 });
